d8f9586327903f2b5a26990dbe2be1cc7b9da960
ekaras <key.cpp@gmail.com>
2022-12-04 18:23:43 +0300

    1

---
+++ SliderSwiftUi/SliderSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUiApp.swift
+//  SliderSwiftUi
+//
+//  Created by Евгений Карась on 4.12.22.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
